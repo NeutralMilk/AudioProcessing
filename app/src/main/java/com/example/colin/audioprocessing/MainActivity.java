f16daab5882f16c7efbcd0e4e16bf36557049cd8
NeutralMilk <c14509343@mydit.ie>
2017-11-20 22:45:04 +0000

    Tidied up graph code

--- app/src/main/java/com/example/colin/audioprocessing/MainActivity.java
+++ app/src/main/java/com/example/colin/audioprocessing/MainActivity.java
@@ -2,29 +2,20 @@ package com.example.colin.audioprocessing;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
-import android.graphics.Canvas;
-import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
-
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
-import com.jjoe64.graphview.series.DataPointInterface;
 import com.jjoe64.graphview.series.LineGraphSeries;
-import com.jjoe64.graphview.series.LineGraphSeries;
-
 import java.util.ArrayList;
-import java.util.concurrent.TimeUnit;
-
 import be.tarsos.dsp.AudioDispatcher;
 import be.tarsos.dsp.AudioEvent;
 import be.tarsos.dsp.AudioProcessor;
@@ -73,21 +64,14 @@ public class MainActivity extends AppCompatActivity
         tb.setText("Record");
         tb.setTextOff("Record");
         tb.setTextOn("Stop");
-        //fGraph.setDrawBackground(true);
+        
+        //graph settings
         fGraph.setBackgroundColor(getResources().getColor(android.R.color.darker_gray));
-
-        //set Scrollable and Scaleable
         fGraph.getViewport().setScalable(true);
-        //fGraph.getViewport().setScalableY(true);
         fGraph.getViewport().setScrollable(true);
-        //fGraph.getViewport().setScrollableY(true);
-
         fGraph.getViewport().setYAxisBoundsManual(true);
         fGraph.getViewport().setMaxY(2500);
         fGraph.getViewport().setMinY(0);
-
-        //fGraph.getViewport().setXAxisBoundsManual(true);
-        //fGraph.getViewport().setMaxX(1000);
         fGraph.getViewport().setMinX(0);
 
         getPitch();
@@ -133,18 +117,6 @@ public class MainActivity extends AppCompatActivity
 
         fGraph.addSeries(xySeries);
 
-        //xySeries.setShape(LineGraphSeries.Shape.POINT);
-        //xySeries.setSize(5);
-
-        //draw a custom shape
-        /*xySeries.setCustomShape(new LineGraphSeries.CustomShape() {
-            @Override
-            public void draw(Canvas canvas, Paint paint, float x, float y, DataPointInterface dataPoint) {
-                paint.setStrokeWidth(10);
-                canvas.drawLine(x+5, y+5, x-5, y-5;, paint);
-                //canvas.drawLine(x, y, x, y, paint);
-            }
-        });*/
     }
 
     //the array needs to be sorted with the x values ascending, otherwise it doesn't work.
@@ -219,6 +191,7 @@ public class MainActivity extends AppCompatActivity
         }
         else if(active)
         {
+            toast = Toast.makeText(getApplicationContext(), "this works", Toast.LENGTH_SHORT);
             active = false;
             audioThread.interrupt();
         }
@@ -257,25 +230,4 @@ public class MainActivity extends AppCompatActivity
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
---
+++ app/src/main/java/com/example/colin/audioprocessing/NoteCalculator.java
@@ -0,0 +1,8 @@
+package com.example.colin.audioprocessing;
+
+/**
+ * Created by Colin on 20/11/2017.
+ */
+
+public class NoteCalculator {
+}
